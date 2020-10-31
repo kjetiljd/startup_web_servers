0989dfd22163d42c2e62c2c085b37885d5f75895
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2020-10-31 15:28:44 +0100

    java/jetty - clarify how to run

--- java/jetty/src/main/java/StartupWebServer.java
+++ java/jetty/src/main/java/StartupWebServer.java
@@ -3,6 +3,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class StartupWebServer {
 
+    // Make sure .../startup_web_servers/java/jetty is your working directory
     public static void main(String[] args) throws Exception {
         Server server = new Server(1337);
         server.setHandler(new WebAppContext("src/main/webapp", "/"));
